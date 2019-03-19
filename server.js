75533ee33d58ecc4c854cf5f15d7606a08a18b7c
Glitch (sayme-whoami) <none>
2019-03-19 14:16:50 +0000

    💾🛰 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,30 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+app.get('/whoami', (request, response) => {
+  const ipaddress = request.headers['x-forwarded-for'].split(',').pop();
+  const language = request.headers['accept-language'];
+  const software = request.headers['user-agent'];
+  response.json({ ipaddress, language, software })
+})
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+  
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
